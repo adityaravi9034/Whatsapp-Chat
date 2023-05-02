39bb85cdd87a9142e6f593c4feb9de01cc873bb2
Aditya Ravi <103438753+adityaravi9034@users.noreply.github.com>
2023-05-02 19:21:45 +0200

    Add files via upload

---
+++ whatsapp-chat-analyzer/app.py
@@ -0,0 +1,120 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import helper
+import preprocessor
+
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+
+    #fetch unique users
+
+    user_list = df['user'].unique().tolist()
+    if 'group_notification' in user_list:
+        user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0, "Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
+
+    if st.sidebar.button("Show Analysis"):
+        st.title("Top Statistics")
+
+        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user, df)
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Word")
+            st.title(words)
+        with col3:
+            st.header("Media Shared")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+
+        #monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'],color = 'green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        #daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='purple')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        #activity map
+        st.title('Activity Map')
+        col1,col2 = st.columns(2)
+        with col1:
+            st.header("Most busy day")
+            busy_day = helper.week_activity_map(selected_user,df)
+            fig,ax = plt.subplots()
+            ax.bar(busy_day.index,busy_day.values)
+            plt.xticks(rotation='vertical')
+
+            st.pyplot(fig)
+        with col2:
+            st.header("Most busy month")
+            busy_month = helper.month_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color = 'orange')
+            plt.xticks(rotation='vertical')
+
+            st.pyplot(fig)
+
+
+        #finding the busiest users in the group(Group Level)
+        if selected_user == 'Overall':
+            st.title('Most Busy Users')
+            x,new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+
+            col1,col2 = st.columns(2)
+            with col1:
+                ax.bar(x.index, x.values, color = 'red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+
+        #WordCloud
+        st.title("Word Cloud")
+        df_wc = helper.create_wordcloud(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        #most common words
+        most_common_df = helper.most_common_words(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.barh(most_common_df[0],most_common_df[1])
+        plt.xticks(rotation = 'vertical')
+        st.title('Most Common Words')
+        st.pyplot(fig)
+
+        """#emoji analysis
+        emoji_df = helper.emoji_helper(selected_user)
+        st.dataframe(emoji_df)"""
+
+
+
+
+
+
+
---
+++ whatsapp-chat-analyzer/preprocessor.py
@@ -0,0 +1,45 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):
+    pattern = r'\[\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}:\d{2}\]\s'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    df['message_date'] = pd.to_datetime(df['message_date'].str.strip(), format='[%d/%m/%y, %H:%M:%S]')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            # user name
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+    df.head()
+
+    df['year'] = df['date'].dt.year
+    df['day_name'] = df['date'].dt.day_name()
+    df['only_date'] = df['date'].dt.date
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    return df
+
+
+
+
+
